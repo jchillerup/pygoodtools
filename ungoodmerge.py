facbd2f3f3a13194c58642952dc6628d75b99e61
Jens Christian Hillerup <jens@hillerup.net>
2012-09-15 17:46:50 +0200

    now supports crude cascading support for priorities

--- ungoodmerge.py
+++ ungoodmerge.py
@@ -4,11 +4,38 @@ import sys, os, glob, string
 import pylzma
 from py7zlib import *
 
-needles = ["[!]", # Perfect dumps
-           "(E)", # Europe
-          ]
+needle_cascade = [
+    "[!]", # We only want perfect dumps
+    ["(E)", "(U)", "(J)"], # We prefer European, but we can live with American dumps if a perfect Euro doesn't exist
+    ]
 
+def reduce_list(inlist, needle):
+    return [x for x in inlist if string.find(x, needle) != -1]
+    
 
+def pick_versions(candidates, cascade):
+    for needle in cascade:
+        # If the needle is a string we must obey
+        if (type(needle) == str):
+            candidates = reduce_list(candidates, needle)
+        elif(type(needle) == list):
+            # We initialize reduced to the empty list because we have an empty candidate set if none of
+            # the priorities are matched.
+            reduced = []
+            
+            for priority in needle:
+                reduced = reduce_list(candidates, priority)
+
+                # If we found at least one match, we're good, otherwise keep looping
+                # BUG: if a ROM contains more than one title, we're not taking those into account here.
+                if (len(reduced) > 0):
+                    break;
+                
+            candidates = reduced
+
+    return candidates
+
+        
 if (len(sys.argv) < 2):
     print "Usage: %s sourcedir [destdir]" % sys.argv[0]
 
@@ -23,19 +50,10 @@ for infile in glob.glob(os.path.join(source_directory, "*.7z")):
     fp = open(infile, "rb")
     rom = Archive7z(fp)
 
-    # Gets all the version from the ROM
-    for version in rom.getnames():
-        this_version_good = True
+    candidates = pick_versions(rom.getnames(), needle_cascade)
 
-        # Make sure the version passes all needles
-        for needle in needles:
-            if (string.find(version, needle) == -1):
-                this_version_good = False
-                break
-
-        if (this_version_good == True):
-            vfp = rom.getmember(version)
-            vout = open(os.path.join(destination_directory, version), "wb")
-            vout.write(vfp.read())
-            print "  - %s" % version
-    
+    for candidate in candidates:
+        vfp = rom.getmember(candidate)
+        vout = open(os.path.join(destination_directory, candidate), "wb")
+        vout.write(vfp.read())
+        print "  - %s" % candidate
